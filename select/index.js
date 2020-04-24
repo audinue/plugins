83b073dd1f69cbaedcffbc7e10526f4608e6116e
Audi Nugraha <audinue@gmail.com>
2020-04-24 05:40:55 +0000

    Create index.js

---
+++ select/index.js
@@ -0,0 +1,20 @@
+function select (selector, attached, detached = () => {}) {
+  new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      const isElement = node => node instanceof HTMLElement
+      const toMatched = (matched, element) => [
+        ...matched,
+        ...(element.matches(selector) ? [element] : []),
+        ...element.querySelectorAll(selector)
+      ]
+      Array.from(mutation.addedNodes)
+        .filter(isElement)
+        .reduce(toMatched, [])
+        .forEach(attached)
+      Array.from(mutation.removedNodes)
+        .filter(isElement)
+        .reduce(toMatched, [])
+        .forEach(detached)
+    })
+  }).observe(document, { childList: true, subtree: true })
+}
